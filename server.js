a98504b72f1f7e1867d7b01fbb46f1b8a1c33911
riyazmohammad <aijazsiddique@gmail.com>
2024-08-15 17:36:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
+const express = require('express');
 const app = require('./app');
 const sequelize = require('./config/database');
 const customersRouter = require('./routes/customers');
 const ordersRouter = require('./routes/orders');
 const authRouter = require('./routes/auth');
-
-
 const port = 3056;
+
+// Logging middleware
+app.use((req, res, next) => {
+  console.log(`Received ${req.method} request for ${req.url}`);
+  next();
+});
+
+// Root path handler
+app.get('/', (req, res) => {
+  res.json({ message: "Welcome to the API" });
+});
 
 // Sync database
 sequelize.sync();
@@ -14,7 +24,6 @@ sequelize.sync();
 app.use('/customers', customersRouter);
 app.use('/orders', ordersRouter);
 app.use('/auth', authRouter);
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
