f9a7fceac625599013d14d8721fb77d81ba10645
riyazmohammad <aijazsiddique@gmail.com>
2024-08-16 19:02:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const sequelize = require('./config/database');
 const customersRouter = require('./routes/customers');
 const ordersRouter = require('./routes/orders');
 const authRouter = require('./routes/auth');
-const port = 3056;
+const port = 3000;
 
 // Detailed logging middleware
 app.use((req, res, next) => {
